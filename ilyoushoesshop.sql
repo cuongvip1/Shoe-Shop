775eeb93c17461264a12a803b5ca955d5006637f
Abbey <128060754+cuongvip1@users.noreply.github.com>
2025-11-15 21:37:13 +0700

    database

--- ilyoushoesshop.sql
+++ ilyoushoesshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 14, 2025 lúc 01:11 AM
+-- Thời gian đã tạo: Th10 15, 2025 lúc 12:35 PM
 -- Phiên bản máy phục vụ: 10.4.32-MariaDB
 -- Phiên bản PHP: 8.0.30
 
@@ -74,7 +74,10 @@ INSERT INTO `don_hang` (`id_don_hang`, `ten_nguoi_nhan`, `sdt`, `dia_chi_nhan`, 
 (1, 'Trần Duy Bảo', '0123456788', 'Tân Phú', 'Giao sớm', '1,431,000 VNĐ', 'Sau khi nhận hàng', 'a:1:{i:4;a:5:{s:10:\"hinh_anh_1\";s:10:\"giay20.jpg\";s:8:\"ten_giay\";s:19:\"Adidas PUREBOOST 21\";s:7:\"don_gia\";s:7:\"1590000\";s:8:\"so_luong\";s:1:\"1\";s:10:\"khuyen_mai\";s:2:\"10\";}}', 'tu_choi', '2025-11-13 10:41:07', '2025-11-13 15:41:36'),
 (3, 'Trần Duy Bảo', '0123456788', 'Tân Phú', 'hihi', '1,020,000 VNĐ', 'AGRIBANK', 'a:1:{i:1;a:5:{s:10:\"hinh_anh_1\";s:10:\"giay13.jpg\";s:8:\"ten_giay\";s:6:\"NMD R2\";s:7:\"don_gia\";s:7:\"1200000\";s:8:\"so_luong\";s:1:\"1\";s:10:\"khuyen_mai\";s:2:\"15\";}}', 'da_huy', '2025-11-13 14:50:36', '2025-11-13 15:48:09'),
 (4, 'Trần Duy Bảo', '0123456788', 'Tân Phú', '11111', '10,200,000 VNĐ', 'Sau khi nhận hàng', 'a:1:{i:1;a:5:{s:10:\"hinh_anh_1\";s:10:\"giay13.jpg\";s:8:\"ten_giay\";s:6:\"NMD R2\";s:7:\"don_gia\";s:7:\"1200000\";s:8:\"so_luong\";i:10;s:10:\"khuyen_mai\";s:2:\"15\";}}', 'da_xac_nhan', '2025-11-13 15:17:09', '2025-11-13 15:49:26'),
-(5, 'Trần Duy Bảo', '0123456788', 'Tân Phú', 'aaaa', '11,220,000 VNĐ', 'Sau khi nhận hàng', 'a:1:{i:1;a:5:{s:10:\"hinh_anh_1\";s:10:\"giay13.jpg\";s:8:\"ten_giay\";s:6:\"NMD R2\";s:7:\"don_gia\";s:7:\"1200000\";s:8:\"so_luong\";i:11;s:10:\"khuyen_mai\";s:2:\"15\";}}', 'cho', '2025-11-13 15:18:44', '2025-11-13 15:18:44');
+(5, 'Trần Duy Bảo', '0123456788', 'Tân Phú', 'aaaa', '11,220,000 VNĐ', 'Sau khi nhận hàng', 'a:1:{i:1;a:5:{s:10:\"hinh_anh_1\";s:10:\"giay13.jpg\";s:8:\"ten_giay\";s:6:\"NMD R2\";s:7:\"don_gia\";s:7:\"1200000\";s:8:\"so_luong\";i:11;s:10:\"khuyen_mai\";s:2:\"15\";}}', 'cho', '2025-11-13 15:18:44', '2025-11-13 15:18:44'),
+(6, 'thoai', '09887654321', 'tân phú', 'Giao Nhanh', '1,798,200 VNĐ', 'Sau khi nhận hàng', 'a:1:{i:2;a:5:{s:10:\"hinh_anh_1\";s:9:\"giay4.jpg\";s:8:\"ten_giay\";s:12:\"Nike Joma IC\";s:7:\"don_gia\";s:6:\"666000\";s:8:\"so_luong\";i:3;s:10:\"khuyen_mai\";s:2:\"10\";}}', 'da_xac_nhan', '2025-11-15 03:35:50', '2025-11-15 03:39:20'),
+(7, 'thoai', '09887654321', 'tân phú', 'hehe', '1,020,000 VNĐ', 'Sau khi nhận hàng', 'a:1:{i:1;a:5:{s:10:\"hinh_anh_1\";s:10:\"giay13.jpg\";s:8:\"ten_giay\";s:6:\"NMD R2\";s:7:\"don_gia\";s:7:\"1200000\";s:8:\"so_luong\";i:1;s:10:\"khuyen_mai\";s:2:\"15\";}}', 'da_huy', '2025-11-15 03:39:50', '2025-11-15 03:40:04'),
+(8, 'thoai', '09887654321', 'tân phú', 'hii', '599,400 VNĐ', 'Sau khi nhận hàng', 'a:1:{i:2;a:5:{s:10:\"hinh_anh_1\";s:9:\"giay4.jpg\";s:8:\"ten_giay\";s:12:\"Nike Joma IC\";s:7:\"don_gia\";s:6:\"666000\";s:8:\"so_luong\";i:1;s:10:\"khuyen_mai\";s:2:\"10\";}}', 'cho', '2025-11-15 03:41:58', '2025-11-15 03:41:58');
 
 -- --------------------------------------------------------
 
@@ -121,8 +124,8 @@ CREATE TABLE `giay` (
 --
 
 INSERT INTO `giay` (`id_giay`, `ten_giay`, `ten_loai_giay`, `ten_thuong_hieu`, `mo_ta`, `don_gia`, `so_luong`, `hinh_anh_1`, `hinh_anh_2`, `hinh_anh_3`, `hinh_anh_4`, `ten_khuyen_mai`, `so_luong_mua`, `created_at`, `updated_at`) VALUES
-(1, 'NMD R2', 'Sandanl', 'Gucci', '<p>✔️ Đế giày được thiết kế chịu ma sát tốt, tăng chiều cao, nhẹ, êm, cân bằng và thoáng khí.&nbsp;</p><p>✔️ Kiểu dáng hottrend của năm nay.&nbsp;</p><p>✔️ Giày đẹp, nhẹ, bền. Có thể làm giày cặp, giày nhóm. Thích hợp đi chơi, chạy bộ, gym, đi học, đi làm...&nbsp;</p><p>✔️ Đế cao su bền chắc, có độ bám cao.</p><p>✔️ Mẫu mới nhất hiện nay mang êm chân thời trang cá tính.&nbsp;</p><p>✔️ Dễ dàng kết hợp với hầu hết các phong cách thời trang như: đi học, đi chơi, đi du lịch. Giày đôi, giày nhóm...</p><p>✔️ Có thể kết hợp với váy, jeans, sooc…. Đều phù hợp!!&nbsp;</p>', '1200000', '79', 'giay13.jpg', 'giay13.jpg', 'giay13.jpg', 'giay13.jpg', 'Ngày lễ', '22', '2021-11-25 07:59:26', '2025-11-13 15:18:44'),
-(2, 'Nike Joma IC', 'Thể thao', 'Nike', '<p>✔️ Đế giày được thiết kế chịu ma sát tốt, tăng chiều cao, nhẹ, êm, cân bằng và thoáng khí.&nbsp;</p><p>✔️ Kiểu dáng hottrend của năm nay.&nbsp;</p><p>✔️ Giày đẹp, nhẹ, bền. Có thể làm giày cặp, giày nhóm. Thích hợp đi chơi, chạy bộ, gym, đi học, đi làm...&nbsp;</p><p>✔️ Đế cao su bền chắc, có độ bám cao.</p><p>✔️ Mẫu mới nhất hiện nay mang êm chân thời trang cá tính.&nbsp;</p><p>✔️ Dễ dàng kết hợp với hầu hết các phong cách thời trang như: đi học, đi chơi, đi du lịch. Giày đôi, giày nhóm...</p><p>✔️ Có thể kết hợp với váy, jeans, sooc…. Đều phù hợp!!&nbsp;</p>', '666000', '100', 'giay4.jpg', 'giay4.jpg', 'giay4.jpg', 'giay4.jpg', 'Mới ra mắt', '0', '2021-11-29 07:59:26', '2021-11-29 07:59:26'),
+(1, 'NMD R2', 'Sandanl', 'Gucci', '<p>✔️ Đế giày được thiết kế chịu ma sát tốt, tăng chiều cao, nhẹ, êm, cân bằng và thoáng khí.&nbsp;</p><p>✔️ Kiểu dáng hottrend của năm nay.&nbsp;</p><p>✔️ Giày đẹp, nhẹ, bền. Có thể làm giày cặp, giày nhóm. Thích hợp đi chơi, chạy bộ, gym, đi học, đi làm...&nbsp;</p><p>✔️ Đế cao su bền chắc, có độ bám cao.</p><p>✔️ Mẫu mới nhất hiện nay mang êm chân thời trang cá tính.&nbsp;</p><p>✔️ Dễ dàng kết hợp với hầu hết các phong cách thời trang như: đi học, đi chơi, đi du lịch. Giày đôi, giày nhóm...</p><p>✔️ Có thể kết hợp với váy, jeans, sooc…. Đều phù hợp!!&nbsp;</p>', '1200000', '78', 'giay13.jpg', 'giay13.jpg', 'giay13.jpg', 'giay13.jpg', 'Ngày lễ', '23', '2021-11-25 07:59:26', '2025-11-15 03:39:50'),
+(2, 'Nike Joma IC', 'Thể thao', 'Nike', '<p>✔️ Đế giày được thiết kế chịu ma sát tốt, tăng chiều cao, nhẹ, êm, cân bằng và thoáng khí.&nbsp;</p><p>✔️ Kiểu dáng hottrend của năm nay.&nbsp;</p><p>✔️ Giày đẹp, nhẹ, bền. Có thể làm giày cặp, giày nhóm. Thích hợp đi chơi, chạy bộ, gym, đi học, đi làm...&nbsp;</p><p>✔️ Đế cao su bền chắc, có độ bám cao.</p><p>✔️ Mẫu mới nhất hiện nay mang êm chân thời trang cá tính.&nbsp;</p><p>✔️ Dễ dàng kết hợp với hầu hết các phong cách thời trang như: đi học, đi chơi, đi du lịch. Giày đôi, giày nhóm...</p><p>✔️ Có thể kết hợp với váy, jeans, sooc…. Đều phù hợp!!&nbsp;</p>', '666000', '96', 'giay4.jpg', 'giay4.jpg', 'giay4.jpg', 'giay4.jpg', 'Mới ra mắt', '4', '2021-11-29 07:59:26', '2025-11-15 03:41:58'),
 (3, 'The Nike Premier II', 'Sneaker', 'Nike', '<p>✔️ Đế giày được thiết kế chịu ma sát tốt, tăng chiều cao, nhẹ, êm, cân bằng và thoáng khí.&nbsp;</p><p>✔️ Kiểu dáng hottrend của năm nay.&nbsp;</p><p>✔️ Giày đẹp, nhẹ, bền. Có thể làm giày cặp, giày nhóm. Thích hợp đi chơi, chạy bộ, gym, đi học, đi làm...&nbsp;</p><p>✔️ Đế cao su bền chắc, có độ bám cao.</p><p>✔️ Mẫu mới nhất hiện nay mang êm chân thời trang cá tính.&nbsp;</p><p>✔️ Dễ dàng kết hợp với hầu hết các phong cách thời trang như: đi học, đi chơi, đi du lịch. Giày đôi, giày nhóm...</p><p>✔️ Có thể kết hợp với váy, jeans, sooc…. Đều phù hợp!!&nbsp;</p>', '999000', '100', 'giay3.jpg', 'giay3.jpg', 'giay3.jpg', 'giay3.jpg', 'Ngày lễ', '0', '2021-12-01 10:21:31', '2021-12-01 10:21:31'),
 (4, 'Adidas PUREBOOST 21', 'Thể thao', 'Adidas', '<p>✔️ Đế giày được thiết kế chịu ma sát tốt, tăng chiều cao, nhẹ, êm, cân bằng và thoáng khí.&nbsp;</p><p>✔️ Kiểu dáng hottrend của năm nay.&nbsp;</p><p>✔️ Giày đẹp, nhẹ, bền. Có thể làm giày cặp, giày nhóm. Thích hợp đi chơi, chạy bộ, gym, đi học, đi làm...&nbsp;</p><p>✔️ Đế cao su bền chắc, có độ bám cao.</p><p>✔️ Mẫu mới nhất hiện nay mang êm chân thời trang cá tính.&nbsp;</p><p>✔️ Dễ dàng kết hợp với hầu hết các phong cách thời trang như: đi học, đi chơi, đi du lịch. Giày đôi, giày nhóm...</p><p>✔️ Có thể kết hợp với váy, jeans, sooc…. Đều phù hợp!!&nbsp;</p>', '1590000', '100', 'giay20.jpg', 'giay20.jpg', 'giay20.jpg', 'giay20.jpg', 'Mới ra mắt', '1', '2021-11-29 07:59:26', '2025-11-13 10:41:07'),
 (5, 'Adidas STAN SMITH', 'Thể thao', 'Adidas', '<p>✔️ Đế giày được thiết kế chịu ma sát tốt, tăng chiều cao, nhẹ, êm, cân bằng và thoáng khí.&nbsp;</p><p>✔️ Kiểu dáng hottrend của năm nay.&nbsp;</p><p>✔️ Giày đẹp, nhẹ, bền. Có thể làm giày cặp, giày nhóm. Thích hợp đi chơi, chạy bộ, gym, đi học, đi làm...&nbsp;</p><p>✔️ Đế cao su bền chắc, có độ bám cao.</p><p>✔️ Mẫu mới nhất hiện nay mang êm chân thời trang cá tính.&nbsp;</p><p>✔️ Dễ dàng kết hợp với hầu hết các phong cách thời trang như: đi học, đi chơi, đi du lịch. Giày đôi, giày nhóm...</p><p>✔️ Có thể kết hợp với váy, jeans, sooc…. Đều phù hợp!!&nbsp;</p>', '1290000', '100', 'giay21.jpg', 'giay21.jpg', 'giay21.jpg', 'giay21.jpg', 'Mới ra mắt', '0', '2021-12-01 10:21:31', '2021-12-01 10:21:31'),
@@ -144,7 +147,8 @@ INSERT INTO `giay` (`id_giay`, `ten_giay`, `ten_loai_giay`, `ten_thuong_hieu`, `
 (21, 'Nike Tiempo Legend', 'Thể thao', 'Nike', '<p>✔️ Đế giày được thiết kế chịu ma sát tốt, tăng chiều cao, nhẹ, êm, cân bằng và thoáng khí.&nbsp;</p><p>✔️ Kiểu dáng hottrend của năm nay.&nbsp;</p><p>✔️ Giày đẹp, nhẹ, bền. Có thể làm giày cặp, giày nhóm. Thích hợp đi chơi, chạy bộ, gym, đi học, đi làm...&nbsp;</p><p>✔️ Đế cao su bền chắc, có độ bám cao.</p><p>✔️ Mẫu mới nhất hiện nay mang êm chân thời trang cá tính.&nbsp;</p><p>✔️ Dễ dàng kết hợp với hầu hết các phong cách thời trang như: đi học, đi chơi, đi du lịch. Giày đôi, giày nhóm...</p><p>✔️ Có thể kết hợp với váy, jeans, sooc…. Đều phù hợp!!&nbsp;</p>', '1990000', '100', 'giay4.jpg', 'giay4.jpg', 'giay4.jpg', 'giay4.jpg', 'Ngày lễ', '0', '2021-11-24 07:59:26', '2021-11-24 07:59:26'),
 (22, 'ADIDAS X Speedflow', 'Thể thao', 'Adidas', '<p>✔️ Đế giày được thiết kế chịu ma sát tốt, tăng chiều cao, nhẹ, êm, cân bằng và thoáng khí.&nbsp;</p><p>✔️ Kiểu dáng hottrend của năm nay.&nbsp;</p><p>✔️ Giày đẹp, nhẹ, bền. Có thể làm giày cặp, giày nhóm. Thích hợp đi chơi, chạy bộ, gym, đi học, đi làm...&nbsp;</p><p>✔️ Đế cao su bền chắc, có độ bám cao.</p><p>✔️ Mẫu mới nhất hiện nay mang êm chân thời trang cá tính.&nbsp;</p><p>✔️ Dễ dàng kết hợp với hầu hết các phong cách thời trang như: đi học, đi chơi, đi du lịch. Giày đôi, giày nhóm...</p><p>✔️ Có thể kết hợp với váy, jeans, sooc…. Đều phù hợp!!&nbsp;</p>', '666000', '100', 'giay12.jpg', 'giay12.jpg', 'giay12.jpg', 'giay12.jpg', 'Sale cuối năm', '0', '2021-12-01 10:21:31', '2021-12-01 10:21:31'),
 (23, 'Jung 96', 'Thể thao', 'New Balance', '<p>✔️ Đế giày được thiết kế chịu ma sát tốt, tăng chiều cao, nhẹ, êm, cân bằng và thoáng khí.&nbsp;</p><p>✔️ Kiểu dáng hottrend của năm nay.&nbsp;</p><p>✔️ Giày đẹp, nhẹ, bền. Có thể làm giày cặp, giày nhóm. Thích hợp đi chơi, chạy bộ, gym, đi học, đi làm...&nbsp;</p><p>✔️ Đế cao su bền chắc, có độ bám cao.</p><p>✔️ Mẫu mới nhất hiện nay mang êm chân thời trang cá tính.&nbsp;</p><p>✔️ Dễ dàng kết hợp với hầu hết các phong cách thời trang như: đi học, đi chơi, đi du lịch. Giày đôi, giày nhóm...</p><p>✔️ Có thể kết hợp với váy, jeans, sooc…. Đều phù hợp!!&nbsp;</p>', '899000', '100', 'giay14.jpg', 'giay14.jpg', 'giay14.jpg', 'giay14.jpg', 'Sale cuối năm', '0', '2021-12-01 10:21:31', '2021-12-01 10:21:31'),
-(24, 'Mercurial Superfly', 'Sneaker', 'Balenciaga', '<p>✔️ Đế giày được thiết kế chịu ma sát tốt, tăng chiều cao, nhẹ, êm, cân bằng và thoáng khí.&nbsp;</p><p>✔️ Kiểu dáng hottrend của năm nay.&nbsp;</p><p>✔️ Giày đẹp, nhẹ, bền. Có thể làm giày cặp, giày nhóm. Thích hợp đi chơi, chạy bộ, gym, đi học, đi làm...&nbsp;</p><p>✔️ Đế cao su bền chắc, có độ bám cao.</p><p>✔️ Mẫu mới nhất hiện nay mang êm chân thời trang cá tính.&nbsp;</p><p>✔️ Dễ dàng kết hợp với hầu hết các phong cách thời trang như: đi học, đi chơi, đi du lịch. Giày đôi, giày nhóm...</p><p>✔️ Có thể kết hợp với váy, jeans, sooc…. Đều phù hợp!!&nbsp;</p>', '666000', '100', 'giay6.jpg', 'giay6.jpg', 'giay6.jpg', 'giay6.jpg', 'Chủ vui nên khuyến mãi', '0', '2021-11-24 07:59:26', '2021-11-24 07:59:26');
+(24, 'Mercurial Superfly', 'Sneaker', 'Balenciaga', '<p>✔️ Đế giày được thiết kế chịu ma sát tốt, tăng chiều cao, nhẹ, êm, cân bằng và thoáng khí.&nbsp;</p><p>✔️ Kiểu dáng hottrend của năm nay.&nbsp;</p><p>✔️ Giày đẹp, nhẹ, bền. Có thể làm giày cặp, giày nhóm. Thích hợp đi chơi, chạy bộ, gym, đi học, đi làm...&nbsp;</p><p>✔️ Đế cao su bền chắc, có độ bám cao.</p><p>✔️ Mẫu mới nhất hiện nay mang êm chân thời trang cá tính.&nbsp;</p><p>✔️ Dễ dàng kết hợp với hầu hết các phong cách thời trang như: đi học, đi chơi, đi du lịch. Giày đôi, giày nhóm...</p><p>✔️ Có thể kết hợp với váy, jeans, sooc…. Đều phù hợp!!&nbsp;</p>', '666000', '100', 'giay6.jpg', 'giay6.jpg', 'giay6.jpg', 'giay6.jpg', 'Chủ vui nên khuyến mãi', '0', '2021-11-24 07:59:26', '2021-11-24 07:59:26'),
+(25, 'Giay Vip', 'Thể thao', 'VietNam', '<p>Giày này xịn</p>', '999000', '100', 'top-15-thuong-hieu-giay-viet-nam-noi-tieng-ban-chay-nhat-h9.jpg', 'top-15-thuong-hieu-giay-viet-nam-noi-tieng-ban-chay-nhat-h9.jpg', 'top-15-thuong-hieu-giay-viet-nam-noi-tieng-ban-chay-nhat-h9.jpg', 'top-15-thuong-hieu-giay-viet-nam-noi-tieng-ban-chay-nhat-h9.jpg', 'Không khuyễn mãi', '0', '2025-11-13 17:24:29', '2025-11-13 17:24:29');
 
 -- --------------------------------------------------------
 
@@ -167,7 +171,7 @@ INSERT INTO `khuyen_mai` (`id_khuyen_mai`, `ten_khuyen_mai`, `gia_tri_khuyen_mai
 (2, 'Ngày lễ', '15'),
 (3, 'Mới ra mắt', '10'),
 (4, 'Sale cuối năm', '5'),
-(5, 'Chủ vui nên khuyến mãi', '3');
+(5, 'Hôm nay vui', '3');
 
 -- --------------------------------------------------------
 
@@ -269,8 +273,7 @@ CREATE TABLE `phan_quyen` (
 
 INSERT INTO `phan_quyen` (`id_phan_quyen`, `ten_phan_quyen`) VALUES
 (1, 'Quản trị viên'),
-(2, 'Người dùng'),
-(3, 'Nhân viên');
+(2, 'Người dùng');
 
 -- --------------------------------------------------------
 
@@ -292,8 +295,10 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('A2QGEMYY3IeKJ0iq3X4QMBMRQpY8ztnJOf8gONx3', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36', 'YTo3OntzOjY6Il90b2tlbiI7czo0MDoidk1sWkR1N2dtWXlmc3ZxRU1QWGJiNDVCTGE2VHJVZHhwVnE1eUlVRCI7czo4OiJnaW9faGFuZyI7YTowOnt9czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMSI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6ODoiRGFuZ05oYXAiO2k6MjtzOjU6ImNoZWNrIjtzOjE6IjIiO3M6OToiZGFuaF9naWFzIjthOjE6e2k6MTthOjU6e3M6MTA6ImhpbmhfYW5oXzEiO3M6MTA6ImdpYXkxMy5qcGciO3M6ODoidGVuX2dpYXkiO3M6NjoiTk1EIFIyIjtzOjc6ImRvbl9naWEiO3M6NzoiMTIwMDAwMCI7czo4OiJzb19sdW9uZyI7aToxMTtzOjEwOiJraHV5ZW5fbWFpIjtzOjI6IjE1Ijt9fX0=', 1763077619),
-('si2ZFpoeHT2R8eU4HOsvQLEySC6jVXKzidgDBMqj', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0', 'YTo2OntzOjY6Il90b2tlbiI7czo0MDoiblJVbFRBQlNOTnZraTFPTmRyS3FYYlpmeU5xVzNDUHM2UGJqR0x4bSI7czo4OiJnaW9faGFuZyI7YTowOnt9czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMSI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NToiY2hlY2siO3M6MToiMiI7czo4OiJEYW5nTmhhcCI7aToyO30=', 1763078977);
+('6J8tkbkihbyQ74E2Wx9tZqlP7dYOFzcSbMip2EyZ', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiWDQ2RkNQbWFwNDFLdDVhNXdQMUxyTGNjdnJCYWd1Ylg1VmF0U3pTVCI7czo4OiJnaW9faGFuZyI7YTowOnt9czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzI6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMS9hdXRoL2xvZ2luIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1763197509),
+('aDViUGGeI3f7cK4C6NtxGIMr1YZAKNToEhMs7WAd', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0', 'YTo2OntzOjY6Il90b2tlbiI7czo0MDoiN3VrZmVSRzc5dUFKTml2aWQ3bllMbW9yc0hwajBoREFpeHBUbHZvdiI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo4OiJnaW9faGFuZyI7YTowOnt9czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMSI7fXM6NToiY2hlY2siO3M6MToiMiI7czo4OiJEYW5nTmhhcCI7aToxMjt9', 1763200560),
+('jVFvAWDeRQ9eiuYx0jKyCYOKvPGJxdL1maBCzVdl', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36', 'YTo2OntzOjY6Il90b2tlbiI7czo0MDoia3VKUGMxOWZzY3V1S1dqZDhuR0Z4cDhUWGZPZHBHMVlsNFF1QTJKWCI7czo4OiJnaW9faGFuZyI7YTowOnt9czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mzc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMS9hZG1pbi9waGFucXV5ZW4iO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjg6IkRhbmdOaGFwIjtpOjE7czo1OiJjaGVjayI7czoxOiIxIjt9', 1763203259),
+('x9uJpWKmV08HrRF6PKYih8IHbORFSbADxe3kf2x7', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0', 'YTo3OntzOjY6Il90b2tlbiI7czo0MDoiU3NiWmtjTTVoT01aMTFkaHo3clAwazdXUzVLZW9wQmVDeUwxUUlsRCI7czo4OiJnaW9faGFuZyI7YTowOnt9czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMSI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6ODoiRGFuZ05oYXAiO2k6MTI7czo1OiJjaGVjayI7czoxOiIyIjtzOjk6ImRhbmhfZ2lhcyI7YToyOntpOjI7YTo1OntzOjEwOiJoaW5oX2FuaF8xIjtzOjk6ImdpYXk0LmpwZyI7czo4OiJ0ZW5fZ2lheSI7czoxMjoiTmlrZSBKb21hIElDIjtzOjc6ImRvbl9naWEiO3M6NjoiNjY2MDAwIjtzOjg6InNvX2x1b25nIjtpOjE7czoxMDoia2h1eWVuX21haSI7czoyOiIxMCI7fWk6MTthOjU6e3M6MTA6ImhpbmhfYW5oXzEiO3M6MTA6ImdpYXkxMy5qcGciO3M6ODoidGVuX2dpYXkiO3M6NjoiTk1EIFIyIjtzOjc6ImRvbl9naWEiO3M6NzoiMTIwMDAwMCI7czo4OiJzb19sdW9uZyI7aToxO3M6MTA6ImtodXllbl9tYWkiO3M6MjoiMTUiO319fQ==', 1763203606);
 
 -- --------------------------------------------------------
 
@@ -318,7 +323,8 @@ INSERT INTO `thuong_hieu` (`id_thuong_hieu`, `ten_thuong_hieu`) VALUES
 (5, 'Puma'),
 (6, 'Vans'),
 (7, 'New Balance'),
-(8, 'Balenciaga');
+(8, 'Balenciaga'),
+(9, 'VietNam');
 
 -- --------------------------------------------------------
 
@@ -343,8 +349,7 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `ten_nguoi_dung`, `email`, `sdt`, `Ten_dang_nhap`, `password`, `id_phan_quyen`) VALUES
 (1, 'Admin', 'admin@gmail.com', '0123456789', 'admin', '$2y$10$UZzte57n35euYs01U./6P.o9X3fv0cklbH7k40/T2SRSvDSnQ6Xem', '1'),
 (2, 'Trần Duy Bảo', 'bao@gmail.com', '0123456788', 'bao', '$2y$10$RNPLYXZ/Fs84IIoK0HPyMOlnhBWcukhXUMVlu.PLEQ4rwKpWSVef.', '2'),
-(3, 'cuong', 'cuong@gmail.com', '0987654321', 'cuong', '$2y$10$GvpGdz7tjY7HJ7PPPFkIJehtVDRD9I85OP2lBXBLnuCtLh8Ejfh1S', '2'),
-(11, 'cuong', 'cuong1@gmail.com', '0987654321', 'cuong1', '$2y$10$zQVD5pfPpIXERRVlvi9YcuPvEBGyM43s6Ac2B5evsUNLUbwMyA1Y.', '2');
+(12, 'thoai', 'thoai@gmail.com', '09887654321', 'thoai', '$2y$10$6JkWljrBXR91xNzI9lyUiOrPkyv7dBDS4kBvnxxCcChR4zVV9MRq2', '2');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -449,7 +454,7 @@ ALTER TABLE `danh_gia`
 -- AUTO_INCREMENT cho bảng `don_hang`
 --
 ALTER TABLE `don_hang`
-  MODIFY `id_don_hang` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_don_hang` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT cho bảng `failed_jobs`
@@ -461,7 +466,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT cho bảng `giay`
 --
 ALTER TABLE `giay`
-  MODIFY `id_giay` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id_giay` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT cho bảng `khuyen_mai`
@@ -497,13 +502,13 @@ ALTER TABLE `phan_quyen`
 -- AUTO_INCREMENT cho bảng `thuong_hieu`
 --
 ALTER TABLE `thuong_hieu`
-  MODIFY `id_thuong_hieu` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_thuong_hieu` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
